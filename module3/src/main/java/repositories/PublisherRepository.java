fa79532a0f461fe773be20e576f34b98fb71519f
karolost23 <karolost12@gmail.com>
2020-06-13 20:01:37 +0200

    Add files via upload

---
+++ module3/src/main/java/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.example.module3.repositories;
+
+import com.example.module3.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
